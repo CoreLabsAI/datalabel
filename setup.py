337397bed13167adbb07e838e60c1371e182cf8e
Isaiah Williams <90869054+DeepTitan@users.noreply.github.com>
2023-02-12 14:00:46 -1000

    fix requirements

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='datalabel',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     include_package_data=True,
     author='TitanLabs',
     author_email='product@titanlabs.co',
     description='quickly and effortlessly edit your dataframes without having to write any code. Its intuitive interface makes it ideal for both experienced data professionals and those new to data editing.',
-    install_requires=['pandas', 'requests', 'fastapi', 'uvicorn'],
+    install_requires=['pandas', 'requests', 'fastapi', 'uvicorn', 'jinja2'],
 )
